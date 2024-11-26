afa421e2905bc874b66f1f2ea4b18747f891df86
aurelien292 <9960@holbertonstudents.com>
2024-11-26 14:27:41 +0100

    Ajout fichier format_Test.c

--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int print_recursive(int num);
+int print_number(int num);
+char print_char(char c);
+int print_string(char *str);
 int _printf(const char *format, ...);
 
 #endif
